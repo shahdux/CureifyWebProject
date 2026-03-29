5f9584a42868d56a955a0de258d7f5117ea49b49
shahdMohammad <shahdmohammadx@gmail.com>
2026-03-29 10:41:37 +0200

    create Navlink Component

---
+++ cureifyproject/src/components/Navlink.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./Navlink.css"
+const Navlink = (props) => {
+    return ( 
+        <>
+          <p className='navLink'>{props.linkname}
+
+        </p>
+        
+        
+        
+        </>
+     );
+}
+ 
+export default Navlink;
