e5527e8983da7e64c16c5088e88fa91b5deb9186
shahdMohammad <shahdmohammadx@gmail.com>
2026-03-29 12:52:13 +0200

    create Header Component

---
+++ cureifyproject/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "./Header.css";
+import homebg from "../assets/homebg.png" ;
+const Header = () => {
+    return ( 
+        <>
+        <div className='header'>
+            {/* <img src={homebg} alt="blue background" /> */}
+
+        </div>
+        
+        
+        
+        </>
+     );
+}
+ 
+export default Header;
