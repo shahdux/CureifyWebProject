fc511b027869cb28282d4a8f557c457d6a7403de
shahdMohammad <shahdmohammadx@gmail.com>
2026-03-29 18:24:52 +0200

    adjust style of filled button component

--- cureifyproject/src/components/Header.jsx
+++ cureifyproject/src/components/Header.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import "./Header.css";
-import homebg from "../assets/homebg.png" ;
+import cureifytext from "../assets/CUREIFY.svg" ;
+import img1 from "../assets/leftimg.png" ;
+import img2 from "../assets/centerimg.svg" ;
+import img3 from "../assets/rightimg.png" ;
+import FilledButton from './FilledButton';
+
+
 const Header = () => {
     return ( 
         <>
         <div className='header'>
             {/* <img src={homebg} alt="blue background" /> */}
+            <img src={cureifytext} alt="cureify text" className='textc'/>
+            <div className='forpg'>
+                <div className='forimages'>
+                    <img src={img1} alt="doctor holding phone" className='pa pleft'/>
+                                        <img src={img2} alt="doctor holding a glass" className='centerimg'/>
+                                                                                <img src={img3} alt="phone app" className='pa pright'/>
+
+
+                </div>
+                <div className='blur'></div>
+                <div className='forherotexts'>
+                    <p className='heroText'>All Your Medications. One Simple App.</p>
+                    <p className='heroSubtext'>Smart reminders, organized schedules, and peace of mind — right in your pocket.</p>
+                    <div className='for2buttons'>
+                        <FilledButton btext="Download Now"/>
+                    </div>
+                </div>
+            </div>
 
         </div>
         
