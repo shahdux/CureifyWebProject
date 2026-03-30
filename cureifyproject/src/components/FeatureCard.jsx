79d0348f80c233c49d2dbcd0a7c206ee50ddc751
shahdMohammad <shahdmohammadx@gmail.com>
2026-03-30 10:12:35 +0200

    add features section

--- cureifyproject/src/components/FeatureCard.jsx
+++ cureifyproject/src/components/FeatureCard.jsx
@@ -3,6 +3,8 @@ import "./FeatureCard.css"
 const FeatureCard = (props) => {
     return ( 
         <>
+       
+
         <div className='fcard'>
             <img src={props.fimg} alt="feature icon" />
             <p className='featureName'>{props.fname}</p>
@@ -10,6 +12,7 @@ const FeatureCard = (props) => {
 
 
         </div>
+        
         
         
         
--- cureifyproject/src/pages/Home.jsx
+++ cureifyproject/src/pages/Home.jsx
@@ -4,6 +4,14 @@ import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import Header2 from '../components/Header2';
 import phone from "../assets/phone.svg";
+import FeatureCard from '../components/FeatureCard';
+import f1 from "../assets/f1.svg";
+import f2 from "../assets/f2.svg";
+import f3 from "../assets/f3.svg";
+import f4 from "../assets/f4.svg";
+
+
+
 const Home = () => {
     return ( 
         <>
@@ -19,9 +27,29 @@ const Home = () => {
             <p className='titles'>It’s the simplicity of staying on track. It’s your health, handled using…</p>
             <div className='forfeatures'>
                 
+<FeatureCard fimg={f1} fname="AI Prescription Scanner" fdes="Stop the manual search struggle. Our AI instantly extracts every detail from your prescription photo,  automatically populating 
+your cart and schedule with 100% accuracy."/>
+<FeatureCard fimg={f2} fname="Adherence Rewards" fdes="Unlike other platforms that only reward purchases, we reward consistency. Earn points for taking your medicine on time and staying on track with your treatment."/>
+<FeatureCard fimg={f3} fname="Real-Time Stock Accuracy" fdes="No more surprise cancellations. Our direct integration with a verified network of pharmacies ensures what you see is actually on the shelf."/>
+<FeatureCard fimg={f4} fname="Blood Test Records" fdes="Stop guessing about your health. Easily upload and compare your blood test results over time to visualize trends and track how your body is responding to treatment."/>
+
+
+        
+        
+
+
+                
+                
+
+                 
+         
+                
+         
+              
             </div>
         </div>
-        
+             
+
         </>
      );
 }
