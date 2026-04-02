6cff341a3722c47764ca13398cb32dfcdf766e22
shahdMohammad <shahdmohammadx@gmail.com>
2026-04-02 22:37:35 +0200

    add mission section

--- cureifyproject/src/pages/About.jsx
+++ cureifyproject/src/pages/About.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import "./About.css"
 import Navbar from '../components/Navbar';
 import about from "../assets/aboutbg.svg";
+import micon from "../assets/micon.svg";
+
 import video from "../assets/video.mp4";
 
 
@@ -13,22 +15,6 @@ const About = () => {
         </div>
 
 
-        {/* <div className='aboutdiv'>
-            <video src={video} autoPlay loop></video>
-              <div className='about-left'>
-                    <h1 className='about-title'>About Cureify</h1>
-                    <p className='about-des'>
-                        Managing medication shouldn't feel overwhelming. Between handwritten prescriptions, 
-                        missed reminders, unavailable medicines, and confusing blood test results — staying 
-                        consistent becomes harder than it should be. In Egypt, many families still rely on 
-                        memory, paper notes, or scattered WhatsApp photos to track important treatments. 
-                        And when life gets busy, health is often the first thing that slips. Keep it emotional. 
-                        Not feature-heavy.
-                    </p>
-                </div>
-
-                    <img src={about} alt="Cureify app on phone" className='about-phone' />
-        </div> */}
     
     
      <div className='aboutdiv'>
@@ -63,7 +49,15 @@ const About = () => {
                 </div>
     </div>
     
-    
+    <div className='centereddiv missionbgimg'>
+        <img src={micon} alt="vision icon" />
+                    <h2 className='mission-title'>Our Mission</h2>
+                    <p className='mission-des'>
+                        "To create a world where managing medication is effortless, accessible, and 
+                        empowering—enabling every person to take control of their health with 
+                        confidence and clarity."
+                    </p>
+    </div>
     
     
     
