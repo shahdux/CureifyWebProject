2d11ed5ac90efbcca612831f7f2f3d5e3fd59ca7
shahdMohammad <shahdmohammadx@gmail.com>
2026-04-03 00:42:07 +0200

    create circular gallery component

--- cureifyproject/src/pages/Careers.jsx
+++ cureifyproject/src/pages/Careers.jsx
@@ -8,6 +8,7 @@ import career4 from "../assets/c4.svg";
 import career5 from "../assets/c5.svg";
 // import career6 from "../assets/c6.svg";
 import FilledButton from '../components/FilledButton';
+import CircularGallery from '../components/CircularGallery';
 
 
 const Careers = () => {
@@ -23,7 +24,7 @@ const Careers = () => {
             <p className='valuedess white'>Managing your medications is simple. Just add your prescriptions, set reminders, and let the app guide you with timely notifications and easy tracking—keeping your health organized and stress-free.</p>
         </div>
     
-        <div className='htwcards'>
+        {/* <div className='htwcards'>
             <img src={career1} alt="" />
                         <img src={career2} alt="" />
             <img src={career3} alt="" />
@@ -31,9 +32,17 @@ const Careers = () => {
             <img src={career5} alt="" />
 
        
-        </div>
+        </div> */}
+       
         <FilledButton btext="View Open Roles"/>
     </div>
+     <div style={{ height: '600px', position: 'relative' }}>
+  <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} scrollEase={0.02}
+ 
+  scrollSpeed={2}
+ 
+/>
+</div>
         </>
     );
 }
