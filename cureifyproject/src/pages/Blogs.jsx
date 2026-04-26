e76012861f3923cde1abf56fbe926da4d3ac25c4
shahdMohammad <shahdmohammadx@gmail.com>
2026-04-26 18:41:10 +0300

    add reveal animatyions in blogs page

--- cureifyproject/src/pages/Blogs.jsx
+++ cureifyproject/src/pages/Blogs.jsx
@@ -1,61 +1,5 @@
-
-// import React, { useEffect, useState } from 'react';
-// import "./Blogs.css";
-// import Navbar from '../components/Navbar';
-// import BlogCard from '../components/BlogCard';
-// import Footer from '../components/Footer';
-// import DownloadApp from '../components/DownloadApp';
-// import { supabase } from './../supabase';
-// import { Link } from 'react-router-dom';
-
-// const Blogs = () => {
-//     const [loading, setLoading] = useState(true);
-//     const [blogs, setBlogs] = useState("");
-
-//     useEffect(() => {
-//         async function callGetAPI() {
-//             const res = await supabase.from("Blogs").select("*");
-//             setBlogs(res.data);
-//             setLoading(false);
-//         }
-//         callGetAPI();
-//     }, []);
-
-//    if (loading) return (
-//     <div className="loader-container">
-//         <div className="spinner"></div>
-//     </div>
-// );
-
-//     return (
-//         <>
-//             <div className='center'>
-//                 <Navbar />
-//             </div>
-
-//             <div className='blogs-container'>
-//                 <div className='blogs-grid'>
-                    
-//                     {blogs.map((blog) => (
-// <Link to={`/blogs/blog${blog.id}`} style={{ textDecoration: "none" }}>
-//                         <BlogCard
-//                             blogimg={blog.image}
-//                             blogtitle={blog.name_en}
-//                             blogdes={blog.description_en}
-//                         />
-//                         </Link>
-//                     ))}
-//                 </div>
-//             </div>
-
-//             <DownloadApp />
-//             <Footer />
-//         </>
-//     );
-// }
-
-// export default Blogs;
 import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import "./Blogs.css";
 import Navbar from '../components/Navbar';
 import BlogCard from '../components/BlogCard';
@@ -64,6 +8,13 @@ import DownloadApp from '../components/DownloadApp';
 import { supabase } from './../supabase';
 import { Link } from 'react-router-dom';
 import { useLang } from '../context/LanguageContext';
+
+const fadeUp = {
+    hidden: { opacity: 0, y: 32 },
+    visible: { opacity: 1, y: 0 }
+};
+
+const vp = { once: true, amount: 0.2 };
 
 const Blogs = () => {
     const [loading, setLoading] = useState(true);
@@ -93,14 +44,23 @@ const Blogs = () => {
 
             <div className='blogs-container'>
                 <div className='blogs-grid'>
-                    {blogs.map((blog) => (
-                        <Link to={`/blogs/blog${blog.id}`} style={{ textDecoration: "none" }} key={blog.id}>
-                            <BlogCard
-                                blogimg={blog.image}
-                                blogtitle={isArabic ? blog.name_ar : blog.name_en}
-                                blogdes={isArabic ? blog.description_ar : blog.description_en}
-                            />
-                        </Link>
+                    {blogs.map((blog, i) => (
+                        <motion.div
+                            key={blog.id}
+                            variants={fadeUp}
+                            initial="hidden"
+                            whileInView="visible"
+                            transition={{ duration: 0.6, delay: i * 0.1 }}
+                            viewport={vp}
+                        >
+                            <Link to={`/blogs/blog${blog.id}`} style={{ textDecoration: "none" }}>
+                                <BlogCard
+                                    blogimg={blog.image}
+                                    blogtitle={isArabic ? blog.name_ar : blog.name_en}
+                                    blogdes={isArabic ? blog.description_ar : blog.description_en}
+                                />
+                            </Link>
+                        </motion.div>
                     ))}
                 </div>
             </div>
